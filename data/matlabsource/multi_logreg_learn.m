5d07c2682bc04af41ba30835cbddce56e12f8c52
Johnny Yan <jjyan@uw.edu>
2013-06-04 16:27:51 -0700

    updated source files for multitask logreg

--- data/matlabsource/multi_logreg_learn.m
+++ data/matlabsource/multi_logreg_learn.m
@@ -1,36 +1,39 @@
-function params = multi_logreg_learn(reg,statesStruct,featuresStruct)
+function params = multi_logreg_learn(reg,states,features)
 % Evaluates the objective (log-likelihood of the data)
 %
 % Usage:
 %
 %    params = multi_logreg_learn(reg,states,features)
-%
-% Find MLE parameters for logistic regression. If M is the number of 
-% users in the dataset, then STATES is a M x 1 array of state labels,
+%% TODO
+% Find MLE parameters for multitask logistic regression. STATES is the If M is the 
+% number of users in the dataset, then STATES is a M x 1 array of state labels,
 % FEATURES is a M x N binary matrix for each user, N is the number of 
 % features
-% The output is the MLE paramters for logistic regression - N x 1 array
+% The output is the MLE paramters for multitask logistic regression
+% N x 1 array - mean weight
 
-T       =   size(featuresStruct,1);     % Number of tasks
-N       =   size(featuresStruct{1},1);  % Number of features
+TASK       =   size(features,1);     % Number of tasks
+FEAT       =   size(features{1},1);  % Number of features
 
-%Optimize likelihood to get parameters%
-paramsopt   =    minFunc(@EvalLikl,zeros(N*T,1));
+%% Optimize log likelihood to get parameters 
+paramsopt   =    minFunc(@EvalLikl,zeros(FEAT*TASK,1));
 
-%Reshape parameters
-params      =   mean(reshape(paramsopt,N,T), 2);
+%% Reshape parameters and return the row mean
+params      =   mean(reshape(paramsopt,FEAT,TASK), 2);
 
     function [logLikl,gradient]=EvalLikl(params)
         logLikl         =   0;
-        gradient        =   zeros(N,T);
-        paramsMat       =   reshape(params,N,T);
+        gradient        =   zeros(FEAT,TASK);
+        paramsMat       =   reshape(params,FEAT,TASK);
         
-        for it = 1:T
-            DN = size(statesStruct{it},2);    % Number of users in Task IT
+        % Loop through all tasks
+        for it = 1:TASK
+            NU = size(states{it},2);    % Number of users in task IT
             subLogLikl = 0;
-            for jt = 1:DN
-                xs           =   featuresStruct{it}(:,jt);
-                ys           =   statesStruct{it}(jt);
+            % Loop through all users in task IT
+            for jt = 1:NU
+                xs           =   features{it}(:,jt);
+                ys           =   states{it}(jt);
 
                 [lik,grad]   =   inferlikelihood(paramsMat(:,it),xs,ys);
 
@@ -38,38 +41,42 @@ params      =   mean(reshape(paramsopt,N,T), 2);
                 gradient(:,it)    =   gradient(:,it)+grad;                
             end
             
-            %Scale Objective and Gradient by number of samples
-            logLikl         =   logLikl + subLogLikl/DN;
-            gradient(:,it)  =   gradient(:,it)/DN;
+            % Scale Objective and Gradient by number of users
+            logLikl         =   logLikl + subLogLikl/NU;
+            gradient(:,it)  =   gradient(:,it)/NU;
         end
         
-        %Negate likelihood/gradient for minimization       
+        % Negate likelihood/gradient for minimization       
         logLikl     =   -logLikl;
         gradient    =   -gradient;
         
-        %Add regularization
+        % Find row mean weight
         paramsAvg = mean(paramsMat, 2);
         
+        % Add regularization to log likelihood
         logLikl     =   logLikl+paramsAvg'*paramsAvg*reg(1)/2;
-        for ii = 1:T
+        for ii = 1:TASK
             diff = paramsMat(:,ii) - paramsAvg;
             logLikl = logLikl + diff'*diff*reg(2)/2;
         end
         
-        m = reg(2)/(reg(1)+T*reg(2));
-        for ii = 1:T
+        % Add regularization to gradient
+        m = reg(2)/(reg(1)+TASK*reg(2));
+        for ii = 1:TASK
             gradient(:,ii) = gradient(:,ii) + paramsAvg*reg(1)*m;
-            for jj = 1:T
+            for jj = 1:TASK
                 diff = paramsMat(:,jj) - paramsAvg;
+                
                 if jj == ii
                     n = 1-m;
                 else
                     n = -m;
                 end    
+                
                 gradient(:,ii) = gradient(:,ii) + diff*reg(2)*n;
             end
         end
-        gradient = reshape(gradient,N*T,1);
+        gradient = reshape(gradient,FEAT*TASK,1);
     end
 
     function [l,g]=inferlikelihood(params,feature,state)
